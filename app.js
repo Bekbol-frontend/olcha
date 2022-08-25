162bd84aa10db9d9c0bbff8d91bedc1ce88ec2de
Bekbol Bekjanov <bekbol.bv@gmail.com>
2022-08-25 21:34:33 +0500

    add tel 7

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const fetchTest = async () => {
-    const response = await fetch(`http://localhost:5000/telefony`)
-    const data = await response.json();
-    console.log(data);
-}
+// const fetchTest = async () => {
+    
+// }
 
-fetchTest();
+// fetchTest();
